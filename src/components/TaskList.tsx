054b7a45dbc7fe159557dca799e490c7ad73687c
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-03-13 20:18:12 -0300

    feat: item is remove action

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -38,6 +38,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksUpdated = tasks.filter((task: Task) => task.id !== id)
+    setTasks(tasksUpdated)
   }
 
   return (
